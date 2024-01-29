c2e206f88e55d96ff60fc0b714b8b2e60cfc8595
VisiteurCode <150355764+VisiteurCode@users.noreply.github.com>
2024-01-29 10:49:27 +0100

    Update streamlit_fake_bills.py

--- streamlit_fake_bills.py
+++ streamlit_fake_bills.py
@@ -7,12 +7,15 @@ st.markdown('Application de détection des faux billets basée sur un modèle de
 
 st.header("Veuillez télécharger un fichier csv ';' contenant les dimensions (mm) des billets...")
 
-uploaded_file = st.file_uploader("Sélectionnez un fichier csv")
+uploaded_file = st.file_uploader("Sélectionnez un fichier csv", type="csv")
 #uploaded_file = pd.read_csv("/home/papa/DataspellProjects/openclassrooms/OpenclassRooms/Projet 10/Data_transformed/billets_compl.csv", sep=';')
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file, sep=';')
-    X = df[['length', 'margin_low', 'margin_up']].copy()
-    st.write(X.sample(10))
+    try:
+        df = pd.read_csv(uploaded_file, sep=';')
+        X = df[['length', 'margin_low', 'margin_up']].copy()
+        st.write(X.sample(10))
+    except:
+        st.error("Vérifier le format du fichier !")
 else:
     st.warning("you need to upload a csv file")
 

e124a5ad4711b005a252d4274c4bea2f550c89e0
VisiteurCode <150355764+VisiteurCode@users.noreply.github.com>
2024-02-03 18:38:51 +0100

    Update fake_bill_prediction.py

--- fake_bill_prediction.py
+++ fake_bill_prediction.py
@@ -5,6 +5,6 @@ import sklearn
 def predict(data, full_data):
     lr_pipeline = joblib.load('./logreg_pipeline.joblib')
     result = lr_pipeline.predict(data)
-    y_pred = pd.DataFrame({'is_genuine': result})
+    y_pred = pd.DataFrame({'Is_Genuine': result})
     pred = pd.concat([y_pred, full_data], axis=1)
     return pred

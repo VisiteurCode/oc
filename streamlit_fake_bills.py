02904be18b7a0ae6e7983e03a808fda4b077535c
VisiteurCode <150355764+VisiteurCode@users.noreply.github.com>
2024-02-03 18:48:46 +0100

    Update streamlit_fake_bills.py

--- streamlit_fake_bills.py
+++ streamlit_fake_bills.py
@@ -3,7 +3,7 @@ import pandas as pd
 from fake_bill_prediction import predict
 
 st.title('Détection des faux billets')
-st.markdown('Application de détection des faux billets basée sur un modèle de régression logistique (avec KNNImputer(n_neighbors=5)')
+st.markdown("Application de détection des faux billets basée sur un modèle de régression logistique (avec KNNImputer(n_neighbors=5). Pour le PoC, affichage  d'un échantillon de 5 lignes.")
 
 st.header("Veuillez télécharger un fichier csv ';' contenant les dimensions (mm) des billets...")
 
